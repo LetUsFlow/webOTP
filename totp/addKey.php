34f04f6bfe271bd688f67ff4a3c22c124a772c72
Florentin Schäfer <7054@htl.rennweg.at>
2020-04-30 15:03:31 +0200

    addKey.php updated

--- totp/addKey.php
+++ totp/addKey.php
@@ -10,9 +10,8 @@ require "TOTP.php";
 
 if (verify()["status"] === "success") $username = verify()["username"];
 else header("Location: ../index.php");
-$db = getPDO();
 
-if (!(isset($_GET["secret"]) && isset($_GET["issuer"]))) {
+if (!isset($_GET["secret"]) || !isset($_GET["issuer"])) {
     die("Error!");
 }
 
@@ -29,14 +28,15 @@ $otp = new Greymich\TOTP\TOTP($secret);
 try {
     $otp->get();
 } catch (Exception $e) {
-    echo "<h2>ERROR!</h2><br><br>";
     echo $e;
     die();
 }
 
 
-$res = $db->query("INSERT INTO totp (username, secret, issuer) VALUES ('$username', '$secret', '$issuer');");
+$stmt = getPDO()->prepare("INSERT INTO totp (username, secret, issuer) VALUES (?, ?, ?);");
+$stmt->execute([$username, $secret, $issuer]);
 
-echo ($res->rowCount() == 1) ? "success" : "error";
 
-echo "<br><br><a href='../dash.php.old'>Done</a>";
+echo ($stmt->rowCount() == 1) ? "success" : "error";
+
+echo "<br><br><a href='../dash.php'>Done</a>";
